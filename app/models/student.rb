195395b0cddc376396501a33c7443d0a0e8a2985
wael <waeelash155@gmail.com>
2022-04-25 15:33:54 -0400

    commit tree f1c4de3ac4af477fa4cbc00d3b78648d126bc3a2

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,5 +3,9 @@ class Student < ApplicationRecord
   def to_s
     "#{first_name} #{last_name}"
   end
-
+   
+  # def students
+  #   students = Student.all
+  #   render json: students
+  # end
 end
